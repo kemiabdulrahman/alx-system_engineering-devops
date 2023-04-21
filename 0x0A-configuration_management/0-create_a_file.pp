f1fa707425417778a009a5d2b8151ece2bb5144b
AbdulRahman MAFE <kemimafe11@gmail.com>
2023-04-21 15:49:50 -0700

    puppet-lint fixed

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,8 @@
 # Creation of a file in the directory /tmp
 
 file = {'/tmp/school':
-
 mode    => '0744',
 owner   => 'www-data',
+group   => 'www-data',
 content => 'I love Puppet',
-group   => 'www-data'
 }
