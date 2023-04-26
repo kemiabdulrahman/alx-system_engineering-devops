64a90f2b7dbcb2f5bfb4cd21a3c0231f149903c8
AbdulRahman MAFE <kemimafe11@gmail.com>
2023-04-27 00:11:48 +0100

    Tasks Done

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,6 +1,6 @@
-# To Install an Nginx server
+# Install Nginx web server (w/ Puppet) making use of puppet
 
-exec {'install':
+exec { 'server configuration':
   provider => shell,
-  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo 'Hello World!' | sudo tee /var/www/html/index.nginx-debian.html ; sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/github.com\/kemiabdulrahman permanent;/" /etc/nginx/sites-available/default ; sudo service nginx start',
+  command  => 'sudo apt-get -y update; sudo apt-get -y install nginx; echo "Hello World!" > /var/www/html/index.html; sudo sed -i "/server_name _;/a location /redirect_me {\\n\\treturn 301 https://google.com;\\n\\t}\\n" /etc/nginx/sites-available/default; sudo service nginx restart',
 }
