094ac29c76c6f13deb4ab6120fc9dd53f5b030d4
AbdulRahman MAFE <kemimafe11@gmail.com>
2023-05-02 12:24:05 +0100

    AbdulRahman MAFE

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,24 @@
+#  Automate the task of creating a custom HTTP header response, but with Puppet.
+exec { 'update':
+  command => 'sudo apt-get update',
+  provider => shell,
+}
+-> package {'nginx':
+  ensure => present,
+}
+-> file_line {'header line'
+  ensure => present,
+  path   => '/etc/nginx/sites-available/default',
+  line   => "    location / {
+  add_header X-Served-By ${hostname};",
+  match  => '^\tlocation / {',
+}
+-> exec { 'restart service':
+  command  => 'sudo service nginx restart',
+  provider => shell,
+}
+
+
+
+
+
