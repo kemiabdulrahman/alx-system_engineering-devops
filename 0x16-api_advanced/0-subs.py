b8caef30e3c1f6b23d24ff9a08ffd45727f3ffef
AbdulRahman MAFE <kemimafe11@gmail.com>
2023-06-06 22:12:56 +0100

    Tasks Done

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
-""" """
+"""Function to query subscribers on a given Reddit subreddit"""
 import requests
+
+
 def number_of_subscribers(subreddit):
-    url = ""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-            "":"",
-            "":""
-
-
+            "User-Agent":"linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
             }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
+        return 0
+    result = response.json().get("data")
+    return result.get("subscribers")
