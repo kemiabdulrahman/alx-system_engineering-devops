9060cf57c5ccd75cd6522f24a502d57dacc2a7a3
AbdulRahman MAFE <kemimafe11@gmail.com>
2023-06-06 22:35:52 +0100

    Tasks Done

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Function to query a list of all hot posts on a given Reddit subreddit"""
+import requests
+
+
+def recurse(subreddit, hot_list=[], after="", count=0):
+    """return list of titles of all hot posts on a given subreddit"""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+            "User-Agent":"linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+            }
+    params = {
+            "after": after,
+            "count": count,
+            "limit": 10
+            }
+    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+    if response.status_code == 404:
+        return None
+
+    results = response.json().get("data")
+    after = results.get("after")
+    count += results.get("dist")
+    for i in results.get("children"):
+        hot_list.append(i.get("data").get("title"))
+
+    if after is not None:
+        return recurse(subreddit, hot_list, after, count)
+    return hot_list
