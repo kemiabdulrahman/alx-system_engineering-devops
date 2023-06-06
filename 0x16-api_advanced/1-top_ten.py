516ad571fea9006fc45b6ab834262c35710538d8
AbdulRahman MAFE <kemimafe11@gmail.com>
2023-06-06 23:46:33 +0100

    Tasks Done

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,12 +7,13 @@ def top_ten(subreddit):
     """Print the titles of the 10 hottest posts on a given subreddit"""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
-            "User-Agent":"linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+            "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
             }
     params = {
             "limit": 10
             }
-    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+    response = requests.get(url, headers=headers, params=params, allow_redirects=False)      # noqa
+
     if response.status_code == 404:
         print(None)
         return
